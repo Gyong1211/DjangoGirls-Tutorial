9d78db42118661f0ac187ec023b720b52f2c9e8b
Gihong Kim <gyong1211@gmail.com>
2017-05-30 15:36:35 +0900

    post_create POST request make Post

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,12 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
+User = get_user_model()
 
 def post_list(request):
     # posts 변수에 ORM을 이용해서 전체 Post의 리스트(쿼리셋를 대입
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-created_date')
     # posts = Post.objects.filter(published_date__lte = timezone.now())
     context = {
         'title': 'PostList from post_list view',
@@ -22,7 +24,18 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html',context=context)
 
 def post_create(request):
-    # post=Post.objects.
-    context = {
-    }
-    return render(request, 'blog/post_create.html',context=context)
+    if request.method == 'GET':
+        context = {
+        }
+        return render(request, 'blog/post_create.html',context=context)
+    elif request.method == 'POST':
+        data = request.POST
+        title = data['title']
+        text = data['text']
+        user = User.objects.first()
+        post = Post.objects.create(
+            author=user,
+            title=title,
+            text=text
+        )
+        return redirect('post_detail', pk=post.id)
