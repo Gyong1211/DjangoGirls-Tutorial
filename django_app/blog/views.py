db0253970039588b32815de72543bb6f0807ac4c
Gihong Kim <gyong1211@gmail.com>
2017-05-30 12:20:01 +0900

    create post_detail view, url

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -6,9 +6,17 @@ from .models import Post
 
 def post_list(request):
     # posts 변수에 ORM을 이용해서 전체 Post의 리스트(쿼리셋를 대입
-    posts = Post.objects.filter(published_date__lte = timezone.now())
+    posts = Post.objects.all()
+    # posts = Post.objects.filter(published_date__lte = timezone.now())
     context = {
         'title': 'PostList from post_list view',
         'posts': posts
     }
     return render(request, 'blog/post_list.html', context=context, )
+
+def post_detail(request, pk):
+    post=Post.objects.get(id=pk)
+    context = {
+        'post':post,
+    }
+    return render(request, 'blog/post_detail.html',context=context)
